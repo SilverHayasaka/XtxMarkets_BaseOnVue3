f906de0e07743a2f8d4f008a4fee9e13d9ff4ac8
SilverHayasaka <silver.zh@hotmail.com>
2023-05-30 10:25:09 +0800

    基本路由

---
+++ src/views/Category/index.vue
@@ -0,0 +1,11 @@
+<template>
+我是分类页面
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<template>
+我是Home页面
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
